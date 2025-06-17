03ccf00790c031a95c0750d0999693984d8192ca
ThanphisitW <kimgongbu2003@gmail.com>
2025-06-17 17:27:28 +0700

    god pls fix this error

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,18 +1,10 @@
 import { OrbitControls, useGLTF } from '@react-three/drei'
 import Rain from './Rain'
-import { useEffect } from 'react'  
 
 useGLTF.preload('/models/Room_project-v1.glb', true)
 
 const Model = () => {
   const { scene } = useGLTF('/models/Room_project-v1.glb')
-
-  useEffect(() => {
-    fetch('/models/Room_project-v1.glb')
-      .then(res => res.text())
-      .then(text => console.log('Model file starts with:', text.slice(0, 100)))
-      .catch(console.error)
-  }, [])
 
   return <primitive object={scene} scale={2} position={[0, -1.5, 0]} />
 }
